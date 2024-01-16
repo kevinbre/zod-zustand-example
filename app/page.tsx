fc07cb9753a593e3fd8b781299a740ef9f204373
kevinbre <b.kevin@hotmail.es>
2024-01-16 16:49:36 -0300

    added documentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useItemsStore } from "./store/items-store";
 
 export default function Home() {
@@ -8,7 +9,10 @@ export default function Home() {
   return (
     <>
       <title>List</title>
-      <div className="text-white flex flex-col min-w-56 items-center justify-center">
+      <div className="text-white flex flex-col min-w-56 items-center justify-center gap-4">
+        <div className="w-full justify-center items-center flex flex-col">
+
+
         <h2 className="bg-purple-700 px-2 w-full">Users</h2>
         <ul className=" text-black w-full">
           {users.map((user, index) => (
@@ -17,6 +21,18 @@ export default function Home() {
             </li>
           ))}
         </ul>
+          </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+            <a href="https://zod.dev/" className="w-52 h-24 border-purple-700 hover:bg-purple-700/20 active:scale-95 border rounded-md flex justify-center items-center transition-all hover:-translate-y-1 gap-2" target="_blank">
+              <Image src="https://zod.dev/logo.svg" alt="Zod logo" width={50} height={50}/> Zod
+            </a>
+            <a href="https://zustand-demo.pmnd.rs/" className="w-52 h-24 border-purple-700 hover:bg-purple-700/20 active:scale-95 border rounded-md flex justify-center items-center transition-all hover:-translate-y-1 gap-2" target="_blank">
+            <img src="https://repository-images.githubusercontent.com/180328715/fca49300-e7f1-11ea-9f51-cfd949b31560" alt="Zod logo" width={100} height={100}/>
+              Zustand
+            </a>
+
+            
+        </div>
       </div>
     </>
   );
