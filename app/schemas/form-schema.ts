94946a3f5bebb86d1a6e381f6c6a61c8b6be0b42
kevinbre <b.kevin@hotmail.es>
2024-01-16 15:55:25 -0300

    added required error on schema

--- app/schemas/form-schema.ts
+++ app/schemas/form-schema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod"
 
 export const formSchema = z.object({
-    firstName: z.string().min(1, "The Name field is required"),
-    lastName: z.string().min(1, "The Lastname field is required")
+    firstName: z.string({required_error: "The Name field is required"}).min(1, "The Name field is required"),
+    lastName: z.string({required_error: "The Lastname field is required"}).min(1, "The Lastname field is required")
 })
 
 export type FormValues = z.infer<typeof formSchema>
